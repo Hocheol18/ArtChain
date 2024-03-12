6ea3955dd867bfcce2c0fd92958f1c2120118304
ParkHocheol <didifia9850@naver.com>
2024-03-12 17:27:45 +0900

    [Feat] ArtCoin Confirm & Funding ArtCoin Receive, Distribute, Refund

--- Contract/Contract_Code/contracts/ERC20MintBurnTransfer.sol
+++ Contract/Contract_Code/contracts/ERC20MintBurnTransfer.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/ERC20.sol)
+
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ArtchainERC20MintBurnable is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Artchain", "ART") {
+contract MintBurnTransferContract is ERC20 {
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
 
+    // 토큰 소각
     function burnTokens(uint256 burnAmount) public {
         _burn(msg.sender, burnAmount);
     }
 
+    // 토큰 전송
     function transferToken(address to, uint256 amount) public {
         transfer(to, amount);
     }
