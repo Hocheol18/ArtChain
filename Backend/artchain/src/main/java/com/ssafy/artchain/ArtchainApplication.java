50e9adf3809fb7a532416f486decda3fd9ff4592
김영욱 <dmnion@naver.com>
2024-03-12 16:15:33 +0900

    초기 세팅입니다

---
+++ Backend/artchain/src/main/java/com/ssafy/artchain/ArtchainApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.artchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtchainApplication.class, args);
+	}
+
+}
