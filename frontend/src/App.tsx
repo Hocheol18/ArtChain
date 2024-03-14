5d5b612b683c979924c3eb918baff37f876740cc
jhahe <jhahe@naver.com>
2024-03-14 16:05:15 +0900

    Feat: 데스크탑/모바일 전체 레이아웃 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { ReactNode } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const Desktop = ({ children }: { children: ReactNode }) => {
+    const isDesktop = useMediaQuery({ minWidth: 501 });
+    return isDesktop ? children : null;
+  };
+  const Mobile = ({ children }: { children: ReactNode }) => {
+    const isMobile = useMediaQuery({ maxWidth: 500 });
+    return isMobile ? children : null;
+  };
 
   return (
     <>
-      <div>
-        <h1>ArtChain</h1>
-      </div>
+      {/* 데스크탑 버전 */}
+      <Desktop>
+        <div style={{ backgroundColor: "#001a38", height: "100dvh" }}>
+          <div
+            style={{
+              width: "390px",
+              margin: "0 auto",
+              backgroundColor: "white",
+              height: "100dvh",
+            }}
+          >
+            <div>ArtChain</div>
+            <div>Desktop</div>
+          </div>
+        </div>
+      </Desktop>
+      <Mobile>
+        <div>ArtChain</div>
+        <div>Mobile</div>
+      </Mobile>
     </>
   );
 }
