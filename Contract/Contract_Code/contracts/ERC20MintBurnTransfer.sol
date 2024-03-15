b393b93563f96de5b39c5c0f2c9aa891774e6d82
ParkHocheol <didifia9850@naver.com>
2024-03-15 17:49:17 +0900

    [feat] Remix update

--- Contract/Contract_Code/contracts/ERC20MintBurnTransfer.sol
+++ Contract/Contract_Code/contracts/ERC20MintBurnTransfer.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/ERC20.sol)
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
