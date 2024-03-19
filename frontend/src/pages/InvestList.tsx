5d228e6adf366608303874d1dbb10799d03925d4
jhahe <jhahe@naver.com>
2024-03-19 13:34:36 +0900

    [feat] FE: 데스크탑에서 이상하게 보이는 거 변경

--- frontend/src/pages/InvestList.tsx
+++ frontend/src/pages/InvestList.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import { TopNavBar } from "../components/Common/Navigation/TopNavBar";
 import { BottomNavBar } from "../components/Common/Navigation/BottomNavBar";
+import { useMediaQuery } from "react-responsive";
 
 export const InvestList = () => {
+  const isDesktop = useMediaQuery({ minWidth: 501 });
+
+  console.log(isDesktop);
   return (
     <div>
       <Box>
         <TopNavBar navType="logo" />
       </Box>
       <div>투자리스트 페이지</div>
-      <Box>
+      <Box position={"fixed"} bottom={0} width={isDesktop ? "390px" : "100%"}>
         <BottomNavBar navType="invest" />
       </Box>
     </div>
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import { TopNavBar } from "../components/Common/Navigation/TopNavBar";
 import { BottomNavBar } from "../components/Common/Navigation/BottomNavBar";
+import { useMediaQuery } from "react-responsive";
 
 export const MainPage = () => {
+  const isDesktop = useMediaQuery({ minWidth: 501 });
   return (
     <div>
       <Box>
         <TopNavBar navType="logo" />
       </Box>
       <div>메인페이지</div>
-      <Box>
+      <Box position={"fixed"} bottom={0} width={isDesktop ? "390px" : "100%"}>
         <BottomNavBar navType="home" />
       </Box>
     </div>
