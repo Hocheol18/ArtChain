55759cd34f74a299f026033765a46388c957623a
김영욱 <dmnion@naver.com>
2024-03-19 17:56:25 +0900

    [feat] BE: cors 수정중
    member에 이미 정보가 있을 때 불러오는거에서 에러나는중
    token expired 값에서 문제가 있는 것 같다

--- Backend/artchain/src/main/java/com/ssafy/artchain/config/CorsMvcConfig.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/config/CorsMvcConfig.java
@@ -14,6 +14,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
     corsRegistry.addMapping("/**")
             .exposedHeaders("Set-Cookie")
-            .allowedOrigins("http://localhost:3000");
+            .allowedOrigins("http://localhost:8080");
   }
 }
--- Backend/artchain/src/main/java/com/ssafy/artchain/config/SecurityConfig.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -51,7 +52,7 @@ public class SecurityConfig {
 
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
                 configuration.setAllowedMethods(Collections.singletonList("*"));
                 configuration.setAllowCredentials(true);
                 configuration.setAllowedHeaders(Collections.singletonList("*"));
@@ -87,4 +88,10 @@ public class SecurityConfig {
 
     return http.build();
   }
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+    return new BCryptPasswordEncoder();
+  }
 }
+
