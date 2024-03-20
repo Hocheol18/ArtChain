5d8f1bd92c500d6f003e1f1424faab0a569477ff
허준혁 <demise1426@naver.com>
2024-03-20 17:15:42 +0900

    [fix] BE: InvestmentLog 연결 Entity 수정

--- Backend/artchain/src/main/java/com/ssafy/artchain/connectentity/InvestmentLog.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/connectentity/InvestmentLog.java
@@ -2,30 +2,58 @@ package com.ssafy.artchain.connectentity;
 
 import com.ssafy.artchain.funding.entity.Funding;
 import com.ssafy.artchain.member.entity.Member;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-    @Getter
-    @Entity
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public class InvestmentLog {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+@Getter
+@ToString
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "investment_log")
+public class InvestmentLog {
 
-        // 외래 키를 지정하되, 제약조건은 걸지 않는다.
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "member_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-        private Member member;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "funding_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-        private Funding funding;
+    // 외래 키를 지정하되, 제약조건은 걸지 않는다.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member member;
 
-        public InvestmentLog(Member member, Funding funding){
-            this.member = member;
-            this.funding = funding;
-        }
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "funding_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Funding funding;
+
+    @Column(name = "transaction_hash")
+    private String transactionHash;
+
+    @Column(name = "transaction_time")
+    private LocalDateTime transactionTime;
+
+    @Column(name = "coin_count")
+    private Long coinCount;
+
+    @Column(name = "piece_count")
+    private Long pieceCount;
+
+}
