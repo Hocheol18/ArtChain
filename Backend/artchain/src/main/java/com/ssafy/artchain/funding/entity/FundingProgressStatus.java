f3d803b7c00e8c150dd6894ac250427032618892
허준혁 <demise1426@naver.com>
2024-03-21 12:24:50 +0900

    [comment] BE: FundingProgressStatus 주석 수정

--- Backend/artchain/src/main/java/com/ssafy/artchain/funding/entity/FundingProgressStatus.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/funding/entity/FundingProgressStatus.java
@@ -2,7 +2,7 @@ package com.ssafy.artchain.funding.entity;
 
 public enum FundingProgressStatus {
     RECRUITMENT_STATUS, // 모집 중
-    PENDING_SETTLEMENT, // 정산 대기
+    PENDING_SETTLEMENT, // 정산 대기(== 모집 성공)
     SETTLED, // 정산 완료
     RECRUITMENT_FAILED, // 모집 실패
 }
