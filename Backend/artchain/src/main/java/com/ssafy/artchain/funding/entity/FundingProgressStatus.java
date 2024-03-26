807b11a57b8cef0edda11468b7272a4e4f6f89dd
허준혁 <demise1426@naver.com>
2024-03-26 11:05:58 +0900

    [feat] BE: FundingProgressStatus 모집 시작 전 상태 추가

--- Backend/artchain/src/main/java/com/ssafy/artchain/funding/entity/FundingProgressStatus.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/funding/entity/FundingProgressStatus.java
@@ -1,6 +1,7 @@
 package com.ssafy.artchain.funding.entity;
 
 public enum FundingProgressStatus {
+    BEFORE_RECRUITMENT, // 모집 시작 전
     RECRUITMENT_STATUS, // 모집 중
     PENDING_SETTLEMENT, // 정산 대기(== 모집 성공)
     SETTLED, // 정산 완료
