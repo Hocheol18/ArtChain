0414ffe1700194984314347f6b501deba0daf33b
김영욱 <dmnion@naver.com>
2024-03-27 09:19:20 +0900

    [comment] BE: 주석 추가

--- Backend/artchain/src/main/java/com/ssafy/artchain/market/dto/MarketMainResponseDto.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/market/dto/MarketMainResponseDto.java
@@ -16,6 +16,7 @@ public class MarketMainResponseDto {
     private String name;
     private String poster;
     private String category;
+    //        모집 완료 된 작품의 모집 완료까지 모인 코인 숫자
     private Long nowCoinCount;
     private LocalDate settlement;
 
@@ -24,6 +25,7 @@ public class MarketMainResponseDto {
         this.name = funding.getName();
         this.poster = funding.getPoster();
         this.category = funding.getCategory();
+//        모집 완료 된 작품의 모집 완료까지 모인 코인 숫자
         this.nowCoinCount = funding.getNowCoinCount();
         this.settlement = funding.getSettlement();
     }
