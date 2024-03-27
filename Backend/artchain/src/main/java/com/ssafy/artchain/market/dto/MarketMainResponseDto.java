f3dd223d0fb928cc80f56864bd244058f6c7a176
김영욱 <dmnion@naver.com>
2024-03-27 09:29:32 +0900

    [poop] BE: 상태 정보 안보내서 수정했음

--- Backend/artchain/src/main/java/com/ssafy/artchain/market/dto/MarketMainResponseDto.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/market/dto/MarketMainResponseDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.artchain.market.dto;
 
 import com.ssafy.artchain.funding.entity.Funding;
+import com.ssafy.artchain.funding.entity.FundingProgressStatus;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ public class MarketMainResponseDto {
     private String name;
     private String poster;
     private String category;
+    private FundingProgressStatus status;
     private Long nowCoinCount;
     private LocalDate settlement;
 
@@ -24,6 +26,7 @@ public class MarketMainResponseDto {
         this.name = funding.getName();
         this.poster = funding.getPoster();
         this.category = funding.getCategory();
+        this.status = funding.getProgressStatus();
         this.nowCoinCount = funding.getNowCoinCount();
         this.settlement = funding.getSettlement();
     }
