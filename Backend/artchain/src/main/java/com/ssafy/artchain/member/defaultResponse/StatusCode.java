af25e4adbc1e642a8585bc471792690988d4a57a
김영욱 <dmnion@naver.com>
2024-03-28 17:51:27 +0900

    [feat] BE: member 관리자 기능 부분 권한 검사 구현

--- Backend/artchain/src/main/java/com/ssafy/artchain/member/defaultResponse/StatusCode.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/member/defaultResponse/StatusCode.java
@@ -26,6 +26,7 @@ public enum StatusCode {
 	// 400 BAD REQUEST : 잘못된 요청 - 요청 구문이 잘못되었음
 //	FAIL_USER_BOOK_DELETE(400, "잘못된 요청으로 인한 사용자 도서 삭제 실패"),
 	FAIL_ALREADY_EXIST_MEMBERID(400, "이미 존재하는 사용자 아이디입니다."),
+	FAIL_NOT_ALLOW(400, "해당 요청에 필요한 권한이 없습니다."),
 
 	// 404 NOT FOUND
 	FAIL_NEW_ACCESS_TOKEN(404, "Access 토큰 재발급 실패");
