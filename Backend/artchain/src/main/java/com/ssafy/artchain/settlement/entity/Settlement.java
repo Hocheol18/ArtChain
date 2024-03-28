69407fc1c5ac23da8c95dc86a6c69e67fb2ba687
허준혁 <demise1426@naver.com>
2024-03-28 15:23:56 +0900

    [feat] BE: Settlement Entity

---
+++ Backend/artchain/src/main/java/com/ssafy/artchain/settlement/entity/Settlement.java
@@ -0,0 +1,47 @@
+package com.ssafy.artchain.settlement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "settlement")
+public class Settlement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "ent_id", nullable = false)
+    private Long entId;
+
+    @Column(name = "funding_id", nullable = false)
+    private Long fundingId;
+
+    @Column(name = "settlement_price", nullable = false)
+    private Long settlementPrice;
+
+    @Column(name = "return_rate", nullable = false)
+    private Integer returnRate;
+
+    @Column(name = "deposite_date", nullable = false)
+    private LocalDate depositeDate;
+
+    @Column(name = "settlement_file", nullable = false)
+    private String settlementFile;
+
+    @Column(name = "status", nullable = false, columnDefinition = "varchar(64)")
+    @Enumerated(EnumType.STRING)
+    private SettlementStatus status = SettlementStatus.REQUEST;
+
+    public void updateStatus(SettlementStatus status) {
+        this.status = status;
+    }
+}
