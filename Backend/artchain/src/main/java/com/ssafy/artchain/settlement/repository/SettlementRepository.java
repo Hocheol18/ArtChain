6f8d4ddb6bd55372e5eba4726682853b3b14a5b0
허준혁 <demise1426@naver.com>
2024-03-29 13:12:58 +0900

    [fix] BE: getSettlement 오류 수정 및 ResponseDto 수정 사항 반영

--- Backend/artchain/src/main/java/com/ssafy/artchain/settlement/repository/SettlementRepository.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/settlement/repository/SettlementRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.artchain.settlement.repository;
 
 import com.ssafy.artchain.settlement.dto.SettlementListItemDto;
+import com.ssafy.artchain.settlement.dto.SettlementResponseDto;
 import com.ssafy.artchain.settlement.entity.Settlement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -24,6 +25,25 @@ public interface SettlementRepository extends JpaRepository<Settlement, Long> {
             "where st.status = :status "
     )
     List<SettlementListItemDto> getSettlementList(@Param("status") String status);
+
+    @Query(value = "select " +
+            "new com.ssafy.artchain.settlement.dto.SettlementResponseDto ( " +
+            "st.id, " +
+            "mb.id, " +
+            "mb.name," +
+            "fd.id," +
+            "fd.name," +
+            "st.settlementPrice," +
+            "st.returnRate," +
+            "st.depositeDate," +
+            "st.settlementFile," +
+            "st.status ) " +
+            "from Settlement st " +
+            "join Member mb on st.entId = mb.id " +
+            "join Funding fd on st.fundingId = fd.id " +
+            "where st.id = :settlementId "
+    )
+    SettlementResponseDto getSettlement(@Param("settlementId") Long settlementId);
 
     @Query("SELECT st.returnRate FROM Settlement st WHERE st.fundingId = :fundingId")
     Integer findReturnRateByFundingId(@Param("fundingId") Long fundingId);
