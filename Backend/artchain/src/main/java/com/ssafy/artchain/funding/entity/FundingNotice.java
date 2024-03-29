60af2e4d70867525a5ff5a7e5548bb7336acea20
허준혁 <demise1426@naver.com>
2024-03-29 10:11:04 +0900

    [feat] BE: FundingNotice BaseTimeEntity 적용

--- Backend/artchain/src/main/java/com/ssafy/artchain/funding/entity/FundingNotice.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/funding/entity/FundingNotice.java
@@ -1,30 +1,21 @@
 package com.ssafy.artchain.funding.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.ssafy.artchain.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Table(name = "funding_notice")
-public class FundingNotice {
+public class FundingNotice extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
