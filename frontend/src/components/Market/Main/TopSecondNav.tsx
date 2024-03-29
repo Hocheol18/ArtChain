e820ee87b737398fd2b0dcc0e652285883c5804c
ParkHocheol <didifia9850@naver.com>
2024-03-29 12:31:15 +0900

    [fix] FE : Bug FIx

--- frontend/src/components/Market/Main/TopSecondNav.tsx
+++ frontend/src/components/Market/Main/TopSecondNav.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 interface Props {
   first: string;
@@ -6,7 +7,7 @@ interface Props {
   third: string;
   forth: string;
   isCheck: boolean;
-  statusTopSecondNav : string;
+  statusTopSecondNav: string;
   setSecondTopNav: (statusTopSecondNav: string) => void;
 }
 
@@ -19,11 +20,122 @@ export default function TopSecondNav({
   statusTopSecondNav,
   setSecondTopNav,
 }: Props) {
-  
+  console.log(statusTopSecondNav)
   return (
     <>
       {isCheck ? (
         <Flex justifyContent={"center"} mt={"0.5rem"} mr={"0.5rem"}>
+          {statusTopSecondNav === "최신순" ? (
+            <Box
+              px={"0.8rem"}
+              py={"0.2rem"}
+              rounded={"0.7rem"}
+              boxShadow={"xl"}
+              fontSize={"xs"}
+              border={"1px"}
+              borderColor={"gray.300"}
+              ml={"0.5rem"}
+            >
+              <Text as={"b"} color={"blue.400"}>
+                {first}
+              </Text>
+            </Box>
+          ) : (
+            <Box
+              px={"0.8rem"}
+              py={"0.2rem"}
+              rounded={"0.7rem"}
+              boxShadow={"xl"}
+              fontSize={"xs"}
+              border={"1px"}
+              borderColor={"gray.300"}
+              ml={"0.5rem"}
+            >
+              <Text
+                as={"b"}
+                color={"gray.400"}
+                onClick={() => {
+                  setSecondTopNav("최신순");
+                }}
+              >
+                {first}
+              </Text>
+            </Box>
+          )}
+          {statusTopSecondNav === "높은가격순" ? (
+            <Box
+              px={"0.8rem"}
+              py={"0.2rem"}
+              rounded={"0.7rem"}
+              boxShadow={"xl"}
+              fontSize={"xs"}
+              border={"1px"}
+              borderColor={"gray.300"}
+              ml={"0.5rem"}
+            >
+              <Text as={"b"} color={"blue.400"}>
+                {second}
+              </Text>
+            </Box>
+          ) : (
+            <Box
+              px={"0.8rem"}
+              py={"0.2rem"}
+              rounded={"0.7rem"}
+              boxShadow={"xl"}
+              fontSize={"xs"}
+              border={"1px"}
+              borderColor={"gray.300"}
+              ml={"0.5rem"}
+            >
+              <Text
+                as={"b"}
+                color={"gray.400"}
+                onClick={() => setSecondTopNav("높은가격순")}
+              >
+                {second}
+              </Text>
+            </Box>
+          )}
+
+          {statusTopSecondNav === "낮은가격순" ? (
+            <Box
+              px={"0.8rem"}
+              py={"0.2rem"}
+              rounded={"0.7rem"}
+              boxShadow={"xl"}
+              fontSize={"xs"}
+              border={"1px"}
+              borderColor={"gray.300"}
+              ml={"0.5rem"}
+            >
+              <Text as={"b"} color={"blue.400"}>
+                {third}
+              </Text>
+            </Box>
+          ) : (
+            <Box
+              px={"0.8rem"}
+              py={"0.2rem"}
+              rounded={"0.7rem"}
+              boxShadow={"xl"}
+              fontSize={"xs"}
+              border={"1px"}
+              borderColor={"gray.300"}
+              ml={"0.5rem"}
+            >
+              <Text
+                as={"b"}
+                color={"gray.400"}
+                onClick={() => setSecondTopNav("낮은가격순")}
+              >
+                {third}
+              </Text>
+            </Box>
+          )}
+        </Flex>
+      ) : (
+        <Flex justifyContent={"center"} mr={"0.5rem"}>
           {statusTopSecondNav === "ALL" ? (
             <Box
               px={"0.8rem"}
@@ -55,117 +167,6 @@ export default function TopSecondNav({
                 color={"gray.400"}
                 onClick={() => {
                   setSecondTopNav("ALL");
-                }}
-              >
-                {first}
-              </Text>
-            </Box>
-          )}
-          {statusTopSecondNav === "PENDING_SETTLEMENT" ? (
-            <Box
-              px={"0.8rem"}
-              py={"0.2rem"}
-              rounded={"0.7rem"}
-              boxShadow={"xl"}
-              fontSize={"xs"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              ml={"0.5rem"}
-            >
-              <Text as={"b"} color={"black.100"}>
-                {second}
-              </Text>
-            </Box>
-          ) : (
-            <Box
-              px={"0.8rem"}
-              py={"0.2rem"}
-              rounded={"0.7rem"}
-              boxShadow={"xl"}
-              fontSize={"xs"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              ml={"0.5rem"}
-            >
-              <Text
-                as={"b"}
-                color={"gray.400"}
-                onClick={() => setSecondTopNav("PENDING_SETTLEMENT")}
-              >
-                {second}
-              </Text>
-            </Box>
-          )}
-
-          {statusTopSecondNav === "SETTLED" ? (
-            <Box
-              px={"0.8rem"}
-              py={"0.2rem"}
-              rounded={"0.7rem"}
-              boxShadow={"xl"}
-              fontSize={"xs"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              ml={"0.5rem"}
-            >
-              <Text as={"b"} color={"black.100"}>
-                {third}
-              </Text>
-            </Box>
-          ) : (
-            <Box
-              px={"0.8rem"}
-              py={"0.2rem"}
-              rounded={"0.7rem"}
-              boxShadow={"xl"}
-              fontSize={"xs"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              ml={"0.5rem"}
-            >
-              <Text
-                as={"b"}
-                color={"gray.400"}
-                onClick={() => setSecondTopNav("SETTLED")}
-              >
-                {third}
-              </Text>
-            </Box>
-          )}
-        </Flex>
-      ) : (
-        <Flex justifyContent={"center"} mr={"0.5rem"}>
-          {statusTopSecondNav === "Total" ? (
-            <Box
-              px={"0.8rem"}
-              py={"0.2rem"}
-              rounded={"0.7rem"}
-              boxShadow={"xl"}
-              fontSize={"xs"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              ml={"0.5rem"}
-            >
-              <Text as={"b"} color={"black.100"}>
-                {first}
-              </Text>
-            </Box>
-          ) : (
-            <Box
-              px={"0.8rem"}
-              py={"0.2rem"}
-              rounded={"0.7rem"}
-              boxShadow={"xl"}
-              fontSize={"xs"}
-              border={"1px"}
-              borderColor={"gray.300"}
-              ml={"0.5rem"}
-            >
-              <Text
-                as={"b"}
-                color={"gray.400"}
-                onClick={() => {
-                  setSecondTopNav("Total");
                 }}
               >
                 {first}
