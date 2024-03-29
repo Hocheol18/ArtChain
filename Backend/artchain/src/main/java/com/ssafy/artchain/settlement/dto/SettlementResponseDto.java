38d0cc918f5e9770a535f680e77f545d34be4c35
허준혁 <demise1426@naver.com>
2024-03-29 13:11:55 +0900

    [feat] BE: SettlementResponseDto 수정

--- Backend/artchain/src/main/java/com/ssafy/artchain/settlement/dto/SettlementResponseDto.java
+++ Backend/artchain/src/main/java/com/ssafy/artchain/settlement/dto/SettlementResponseDto.java
@@ -2,15 +2,21 @@ package com.ssafy.artchain.settlement.dto;
 
 import com.ssafy.artchain.settlement.entity.Settlement;
 import com.ssafy.artchain.settlement.entity.SettlementStatus;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SettlementResponseDto {
     private Long id;
     private Long entId;
+    private String entName;
     private Long fundingId;
+    private String fundingTitle;
     private Long settlementPrice;
     private Integer returnRate;
     private LocalDate depositeDate;
