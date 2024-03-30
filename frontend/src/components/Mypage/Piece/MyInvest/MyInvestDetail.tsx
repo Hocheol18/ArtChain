3fad53a8da4624589c9b7721c2c469d81c5ab043
JHyeon-a <jhahe78@gmail.com>
2024-03-31 02:28:38 +0900

    [feat] FE: 나의 조각(마이) 도넛 모양 차트 변경 및 나의 투자/ 나의 거래 로직 변경, filter 공통 컴포넌트로 빼서 활용 등 UI / 나의 투자 완료

--- frontend/src/components/Mypage/Piece/MyInvest/MyInvestDetail.tsx
+++ frontend/src/components/Mypage/Piece/MyInvest/MyInvestDetail.tsx
@@ -1,323 +1,139 @@
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
   Box,
   Center,
   Flex,
   Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 
-import falloutboy from "../../../../assets/falloutboy.png";
+import TmpImg from "../../../../assets/invest-poster-tmp-img.jpg";
 import puzzle from "../../../../assets/puzzle.svg";
 import CommonNotice from "../../Business/CommonNotice";
+import { useEffect, useState } from "react";
 
 interface Props {
   isNow: string;
 }
 
 export default function MyInvestDetail({ isNow }: Props) {
+  // statusBadge: 진행중, 모집종료, 정산완료
+  const [statusBadge, setStatusBadge] = useState("");
+  // 색깔
+  const [statusColor, setStatusColor] = useState("blue.300");
+
+  //상태가 바뀔 때마다
+  useEffect(() => {
+    switch (isNow) {
+      case "now":
+        setStatusColor("blue.300");
+        setStatusBadge("진행중");
+        break;
+      case "wait":
+        setStatusColor("gray.500");
+        setStatusBadge("모집종료");
+        break;
+      case "end":
+        setStatusColor("blue.200");
+        setStatusBadge("정산완료");
+        break;
+    }
+  }, [isNow]);
+
   return (
     <>
-      {isNow === "now" ? (
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as="span" flex="1" textAlign="left">
-                  <Flex
-                    ml={"0.5rem"}
-                    mt={"0.5rem"}
-                    mb={"0.5rem"}
-                    w={"100%"}
-                    maxH={"160px"}
-                  >
-                    <Image w={"100px"} h={"150px"} src={falloutboy} />
+      <Flex
+        direction={"column"}
+        border={"1px solid"}
+        borderColor={"gray.300"}
+        borderRadius={"3xl"}
+        px={5}
+        pb={5}
+      >
+        <Flex py={4}>
+          <Image src={TmpImg} h={100} w={65} />
 
-                    <Flex direction={"column"} ml={"0.3rem"}>
-                      <Box w={"40%"}>
-                        <CommonNotice
-                          first="진행중"
-                          second=""
-                          thrid=""
-                          isNow={isNow}
-                        />
-                      </Box>
-                      <Flex
-                        direction={"column"}
-                        justifyContent={"space-between"}
-                        h={"100%"}
-                      >
-                        <Text
-                          ml={"0.5rem"}
-                          as={"b"}
-                          fontSize={"1.2rem"}
-                          mt={"0.2rem"}
-                        >
-                          Fall Out Boy :: 2024 Seoul Concert
-                        </Text>
-                        <Box
-                          bgColor={"blue.100"}
-                          w={"50%"}
-                          borderRadius={"1rem"}
-                          ml={"0.5rem"}
-                          borderColor={"blue.100"}
-                          border={"1px"}
-                          mb={"0.3rem"}
-                        >
-                          <Center
-                            ml={"0.3rem"}
-                            mr={"0.3rem"}
-                            mt={"0.1rem"}
-                            mb={"0.1rem"}
-                          >
-                            <Image
-                              boxSize={"1.3rem"}
-                              src={puzzle}
-                              color={"blue.400"}
-                            ></Image>
-                            <Text
-                              as={"b"}
-                              color={"blue.400"}
-                              fontSize={"1rem"}
-                              ml={"0.2rem"}
-                            >
-                              10 조각
-                            </Text>
-                          </Center>
-                        </Box>
-                      </Flex>
-                    </Flex>
-                  </Flex>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <Box bgColor={"gray.100"}>
-              <AccordionPanel pb={4}>
-                <Flex justifyContent={"space-between"}>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      예상 정산가
-                    </Text>
-                    <Text fontSize={"0.9rem"}>10.54 아트</Text>
-                  </Flex>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      조각 당 평단가
-                    </Text>
-                    <Text fontSize={"0.9rem"}>1.3 아트</Text>
-                  </Flex>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      정산일
-                    </Text>
-                    <Text fontSize={"0.9rem"}>2024.04.05(금)</Text>
-                  </Flex>
-                </Flex>
-              </AccordionPanel>
+          <Flex direction={"column"} pl={3} gap={0.5}>
+            <Flex>
+              <Box
+                backgroundColor={statusColor}
+                borderRadius={17}
+                px={3}
+                py={1}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                boxShadow={"base"}
+              >
+                <Text fontSize={12} color={"white"}>
+                  {statusBadge}
+                </Text>
+              </Box>
+              <Box flex={1}></Box>
+            </Flex>
+            <Box fontWeight={"bold"}>
+              Fall Out Boy : 2024 Seoul sssa Concert sdfffffffffff
+              sdfffffffffffdsf dsfsfsfsfsf
             </Box>
-          </AccordionItem>
-        </Accordion>
-      ) : isNow === "wait" ? (
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as="span" flex="1" textAlign="left">
-                  <Flex
-                    ml={"0.5rem"}
-                    mt={"0.5rem"}
-                    mb={"0.5rem"}
-                    w={"100%"}
-                    maxH={"160px"}
-                  >
-                    <Image w={"100px"} h={"150px"} src={falloutboy} />
-
-                    <Flex direction={"column"}>
-                      <Box w={"40%"}>
-                        <CommonNotice
-                          first=""
-                          second="모집종료"
-                          thrid=""
-                          isNow={isNow}
-                        />
-                      </Box>
-                      <Flex
-                        direction={"column"}
-                        justifyContent={"space-between"}
-                        h={"100%"}
-                      >
-                        <Text
-                          ml={"0.5rem"}
-                          as={"b"}
-                          fontSize={"1.2rem"}
-                          mt={"0.2rem"}
-                        >
-                          Fall Out Boy :: 2024 Seoul Concert
-                        </Text>
-                        <Box
-                          bgColor={"blue.100"}
-                          w={"50%"}
-                          borderRadius={"1rem"}
-                          ml={"0.5rem"}
-                          borderColor={"blue.100"}
-                          border={"1px"}
-                          mb={"0.3rem"}
-                        >
-                          <Center
-                            ml={"0.3rem"}
-                            mr={"0.3rem"}
-                            mt={"0.1rem"}
-                            mb={"0.1rem"}
-                          >
-                            <Image
-                              boxSize={"1.3rem"}
-                              src={puzzle}
-                              color={"blue.400"}
-                            ></Image>
-                            <Text
-                              as={"b"}
-                              color={"blue.400"}
-                              fontSize={"1rem"}
-                              ml={"0.2rem"}
-                            >
-                              10 조각
-                            </Text>
-                          </Center>
-                        </Box>
-                      </Flex>
-                    </Flex>
-                  </Flex>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <Box bgColor={"gray.100"}>
-              <AccordionPanel pb={4}>
-                <Flex justifyContent={"space-between"}>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      예상 정산가
-                    </Text>
-                    <Text fontSize={"0.9rem"}>10.54 아트</Text>
-                  </Flex>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      조각 당 평단가
-                    </Text>
-                    <Text fontSize={"0.9rem"}>1.3 아트</Text>
-                  </Flex>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      정산일
-                    </Text>
-                    <Text fontSize={"0.9rem"}>2024.04.05(금)</Text>
-                  </Flex>
-                </Flex>
-              </AccordionPanel>
-            </Box>
-          </AccordionItem>
-        </Accordion>
-      ) : (
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as="span" flex="1" textAlign="left">
-                  <Flex
-                    ml={"0.5rem"}
-                    mt={"0.5rem"}
-                    mb={"0.5rem"}
-                    w={"100%"}
-                    maxH={"160px"}
-                  >
-                    <Image w={"100px"} h={"150px"} src={falloutboy} />
-
-                    <Flex direction={"column"}>
-                      <Box w={"40%"}>
-                        <CommonNotice
-                          first=""
-                          second=""
-                          thrid="정산완료"
-                          isNow={isNow}
-                        />
-                      </Box>
-                      <Flex
-                        direction={"column"}
-                        justifyContent={"space-around"}
-                        h={"100%"}
-                      >
-                        <Text ml={"0.5rem"} as={"b"} fontSize={"1.2rem"}>
-                          Fall Out Boy :: 2024 Seoul Concert
-                        </Text>
-                        <Box
-                          bgColor={"blue.100"}
-                          w={"50%"}
-                          borderRadius={"1rem"}
-                          ml={"0.5rem"}
-                          borderColor={"blue.100"}
-                          border={"1px"}
-                          mb={"0.3rem"}
-                        >
-                          <Center
-                            ml={"0.3rem"}
-                            mr={"0.3rem"}
-                            mt={"0.1rem"}
-                            mb={"0.1rem"}
-                          >
-                            <Image
-                              boxSize={"1.3rem"}
-                              src={puzzle}
-                              color={"blue.400"}
-                            ></Image>
-                            <Text
-                              as={"b"}
-                              color={"blue.400"}
-                              fontSize={"1rem"}
-                              ml={"0.2rem"}
-                            >
-                              10 조각
-                            </Text>
-                          </Center>
-                        </Box>
-                      </Flex>
-                    </Flex>
-                  </Flex>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <Box bgColor={"gray.100"}>
-              <AccordionPanel pb={4}>
-                <Flex justifyContent={"space-between"}>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      예상 정산가
-                    </Text>
-                    <Text fontSize={"0.9rem"}>10.54 아트</Text>
-                  </Flex>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      조각 당 평단가
-                    </Text>
-                    <Text fontSize={"0.9rem"}>1.3 아트</Text>
-                  </Flex>
-                  <Flex direction={"column"}>
-                    <Text fontSize={"1rem"} as={"b"}>
-                      정산일
-                    </Text>
-                    <Text fontSize={"0.9rem"}>2024.04.05(금)</Text>
-                  </Flex>
-                </Flex>
-              </AccordionPanel>
-            </Box>
-          </AccordionItem>
-        </Accordion>
-      )}
+            <Flex>
+              <Box
+                display={"flex"}
+                alignItems={"center"}
+                backgroundColor={"blue.100"}
+                color={"blue.300"}
+                px={1}
+                borderRadius={"md"}
+              >
+                <Image
+                  src={puzzle}
+                  w={22}
+                  h={22}
+                  filter={
+                    "invert(19%) sepia(100%) saturate(1891%) hue-rotate(201deg) brightness(90%) contrast(99%)"
+                  }
+                />
+                <Box>10 조각</Box>
+              </Box>
+              <Box flex={1}></Box>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex>
+          <Flex direction={"column"} flex={1}>
+            <Box fontWeight={"bold"}>지분율</Box>
+            <Box>88.89%</Box>
+          </Flex>
+          <Flex direction={"column"} flex={1}>
+            <Box fontWeight={"bold"}>1조각 평단가</Box>
+            <Box>3.5 아트</Box>
+          </Flex>
+          <Flex direction={"column"} flex={1}>
+            <Box fontWeight={"bold"}>정산일</Box>
+            <Box>2024.04.05(금)</Box>
+          </Flex>
+        </Flex>
+        {isNow === "end" ? (
+          <Flex mt={2}>
+            <Flex direction={"column"} flex={1}>
+              <Box fontWeight={"bold"}>정산 코인</Box>
+              <Box>12,132 아트</Box>
+            </Flex>
+            <Flex direction={"column"} flex={1}>
+              <Box fontWeight={"bold"}>최종 수익률</Box>
+              <Box>24%</Box>
+            </Flex>
+            <Flex flex={1}></Flex>
+          </Flex>
+        ) : null}
+      </Flex>
     </>
   );
 }
--- frontend/src/pages/UserMyPage.tsx
+++ frontend/src/pages/UserMyPage.tsx
@@ -1,22 +1,12 @@
 import { Box, Button, Text, Image, Flex, Center } from "@chakra-ui/react";
 
-import wallet from "../assets/wallet.svg";
-import copy from "../assets/copy.svg";
-import puzzle from "../assets/puzzle.svg";
-import rightarrow from "../assets/rightarrow.svg";
-
-import BackIcon from "../assets/back-icon.svg";
 import { useNavigate, Link } from "react-router-dom";
 import DoughnutChart from "../components/Mypage/DoughnutChart";
-
+import { MyPiece } from "./MyPiece";
 
 export default function UserMyPage() {
   const num = 100;
   const navigate = useNavigate();
-  const handleBack = () => {
-    navigate(-1); //뒤로가기
-  };
-
   const handleCharge = () => {
     navigate("/charge");
   };
@@ -25,183 +15,58 @@ export default function UserMyPage() {
     navigate("/exchange");
   };
 
+  //임시
+  const data = [
+    {
+      title: "Hello Kitty 전시회와 뮤지컬",
+      percent: 40,
+    },
+    {
+      title: "씬",
+      percent: 35,
+    },
+    {
+      title: "디어 에반 헨스",
+      percent: 20,
+    },
+    {
+      title: "그 외",
+      percent: 5,
+    },
+  ];
+
+  const colorArr = ["blue.400", "blue.300", "blue.200", "gray.300"];
+
   return (
     <>
-      <Box
-        bgGradient={"linear(to-r, #16d9e3 0%, #30c7ec 15%, #46aef7 80%)"}
-        minW={"360px"}
-        h={"380px"}
-        borderRadius={"1rem"}
-        boxShadow={"xl"}
-      >
-        <Box height={50} pl={30} display="flex" alignItems={"center"}>
-          <Button variant="unstyled" onClick={handleBack}>
-            <Image src={BackIcon} />
-          </Button>
+      <Flex direction={"column"} backgroundColor={"gray.100"} pb={6}>
+        <Box fontSize={20} fontWeight={"bold"} mx={5} mt={5} mb={3}>
+          현재 투자 중인 작품
         </Box>
-        <Box>
-          {/* <Flex mt={"0.5rem"}>
-            <Text as={"b"} fontSize={"1.7rem"} ml={"2rem"}>
-              김지은님
-            </Text>
-            {userInfo.isLogin ? (
-              <Image
-                boxSize={"1rem"}
-                src={settings}
-                ml={"0.5rem"}
-                mt={"0.8rem"}
-                onClick={() =>
-                  LogoutAxios()
-                    .then(() => clearFunction())
-                    .catch(() =>
-                      toast({
-                        duration: 2000,
-                        isClosable: true,
-                        position: "top",
-                        render: () => (
-                          <Flex
-                            color="white"
-                            mt={"50px"}
-                            bg="#C70000"
-                            p={"1rem"}
-                            borderRadius={"0.7rem"}
-                            alignItems={"center"}
-                          >
-                            <CheckCircleIcon
-                              boxSize={5}
-                              color={"white"}
-                              ml={"0.5rem"}
-                            />
-                            <Center ml={"1rem"}>
-                              <Text as={"b"}>로그아웃 실패</Text>
-                            </Center>
-                          </Flex>
-                        ),
-                      })
-                    )
-                }
-              />
-            ) : null}
-          </Flex> */}
-          <Flex ml={"1.5rem"}>
-            <Image boxSize={"1rem"} src={wallet} ml={"0.5rem"} mt={"0.8rem"} />
-            <Text fontSize={"1rem"} ml={"0.8rem"} mt={"0.7rem"}>
-              0x67F07AFa...
-            </Text>
-            <Image boxSize={"1rem"} src={copy} ml={"0.3rem"} mt={"0.9rem"} />
+        <Flex alignItems={"center"}>
+          <Flex justifyContent={"center"} boxSize={"35%"} mx={6}>
+            <DoughnutChart data={data} />
           </Flex>
-        </Box>
-        <Center h={"150px"}>
-          <Text as={"b"} fontSize={"2.5rem"}>
-            10,000
-          </Text>
-          <Text as={"b"} fontSize={"2.5rem"} ml={"1rem"}>
-            아트
-          </Text>
-        </Center>
-        <Flex justifyContent={"center"}>
-          <Box
-            minW={"140px"}
-            py={"0.7rem"}
-            rounded={"0.7rem"}
-            mt={"0.5rem"}
-            fontSize={"sm"}
-            bgColor={"blue.300"}
-            onClick={handleCharge}
-          >
-            <Center color={"white.100"}>충전하기</Center>
-          </Box>
-          <Box
-            minW={"140px"}
-            py={"0.7rem"}
-            rounded={"0.7rem"}
-            mt={"0.5rem"}
-            fontSize={"sm"}
-            bgColor={"white.100"}
-            ml={"2rem"}
-            onClick={handleExchange}
-          >
-            <Center color={"blue.400"}>환전하기</Center>
-          </Box>
+          <Flex direction={"column"} mr={6} ml={2}>
+            {data.map((item, index) => (
+              <>
+                <Flex alignItems={"center"} py={1}>
+                  <Box
+                    w={"20px"}
+                    h={"20px"}
+                    borderRadius={"5px"}
+                    bgColor={colorArr[index]}
+                  />
+                  <Text ml={"0.5rem"} fontSize={15}>
+                    {item.title}
+                  </Text>
+                </Flex>
+              </>
+            ))}
+          </Flex>
         </Flex>
-      </Box>
-      <Flex
-        mt={"1rem"}
-        p={"1rem"}
-        ml={"1rem"}
-        justifyContent={"space-between"}
-        minW={"360px"}
-        as={Link}
-        to={"/mypiece/1"}
-      >
-        <Flex>
-          <Image boxSize={"2.7rem"} src={puzzle}></Image>
-          <Text as={"b"} color={"black.100"} fontSize={"2rem"} ml={"1rem"}>
-            나의 조각
-          </Text>
-        </Flex>
-
-        <Image boxSize={"2.7rem"} src={rightarrow}></Image>
       </Flex>
-      <Box ml={"2rem"}>
-        <Box mt={"2.5rem"} h={"50px"}>
-          <Text as={"b"} color={"black.100"} fontSize={"2rem"}>
-            투자 요약
-          </Text>
-        </Box>
-        <Flex direction={"column"} mt={"0.5rem"}>
-          <Text as={"b"} fontSize={"1.2rem"}>
-            전체 투자 횟수 : {num}회
-          </Text>
-          <Flex mt={"0.3rem"}>
-            <Box
-              w={"25px"}
-              h={"25px"}
-              borderRadius={"5px"}
-              bgColor={"blue.100"}
-            />
-            <Text fontSize={"1.2rem"} ml={"0.5rem"}>
-              모집 중 : {num}회
-            </Text>
-          </Flex>
-          <Flex mt={"0.3rem"}>
-            <Box
-              w={"25px"}
-              h={"25px"}
-              borderRadius={"5px"}
-              bgColor={"blue.200"}
-            />
-            <Text fontSize={"1.2rem"} ml={"0.5rem"}>
-              정산 대기 : {num}회
-            </Text>
-          </Flex>
-          <Flex mt={"0.3rem"}>
-            <Box
-              w={"25px"}
-              h={"25px"}
-              borderRadius={"5px"}
-              bgColor={"blue.300"}
-            />
-            <Text fontSize={"1.2rem"} ml={"0.5rem"}>
-              정산 완료 : {num}회
-            </Text>
-          </Flex>
-          <Flex mt={"0.3rem"}>
-            <Box
-              w={"25px"}
-              h={"25px"}
-              borderRadius={"5px"}
-              bgColor={"gray.300"}
-            />
-            <Text fontSize={"1.2rem"} ml={"0.5rem"}>
-              모집 실패 : {num}회
-            </Text>
-          </Flex>
-        </Flex>
-      </Box>
-      <Flex justifyContent={"center"} p={"4rem"}>
-        <DoughnutChart />
-      </Flex>
+      <MyPiece type="invest" />
     </>
   );
 }
